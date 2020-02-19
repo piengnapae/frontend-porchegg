f8476b11b86175e78be0a9ab06d56204577f0b41
chenchira koedbuengphrao <chenchirakoed@gmail.com>
2020-02-19 17:33:00 +0700

    update

--- src/renderer/pages/member.vue
+++ src/renderer/pages/member.vue
@@ -353,7 +353,30 @@ import Folder from '../components/folder'
         })       
       }, 
       dialogFormVisibles(){
-          axios.post(this.server_api+'/V1/requests/', {
+       let inputHead = this.inputHeader  
+       inputHead[0]['type'] = 'text'
+       let inputAuth =  {}
+        if(this.auth == "Bearer Token"){
+        inputAuth = 
+       {
+        "auth": {
+         "type": "Bearer Token",
+         "bearer": [
+          {
+           "key": "token",
+           "value": this.token,
+           "type": "string"
+          }
+         ]
+        }
+       }   
+      }else  
+      inputAuth = 
+       {
+        "auth": {}
+       }  
+      
+          axios.post(this.server_api+'/requests/', {
           name: this.saverequest.name,
           id_folder: 1,
 	        method: this.method,
